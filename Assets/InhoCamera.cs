d04b01eb086123a40e5400010848cdb7cd237934
qweasfjbv <qweasfjbv@naver.com>
2020-07-20 17:46:43 +0900

    inho

---
+++ Assets/InhoCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InhoCamera : MonoBehaviour
+{
+
+    Moving player;
+
+    Vector3 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Moving>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vector = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        this.transform.position = vector;
+    }
+}
