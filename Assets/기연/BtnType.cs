c1f13e8618e61dc0f77385bdf279953cc2e64507
gi-yeon <63438385+gi-yeon@users.noreply.github.com>
2020-07-23 03:18:05 +0900

    giyeon-07-23-메뉴창 초기

---
+++ Assets/기연/BtnType.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class BtnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Transform buttonScale;
+    Vector3 defaultScale;
+    public BTNType currentType;
+
+    private void Start()
+    {
+        defaultScale = buttonScale.localScale;
+    }
+    public void OnBtnClick()
+    {
+        switch (currentType)
+        {
+            case BTNType.newStart:
+                Debug.Log("New Start");
+                break;
+            case BTNType.load:
+                Debug.Log("Load Character");
+                break;
+
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale*1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonScale.localScale = defaultScale;
+    }
+}
