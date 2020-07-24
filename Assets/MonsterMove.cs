f5c0df0b24ec2ffa634b47ebe23b79f3d36a616d
joonsang20 <dbwnstkd0902@naver.com>
2020-07-24 21:06:45 +0900

    asdf

---
+++ Assets/MonsterMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public int nextMove;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        Invoke("Think", 1);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(nextMove, rigid.velocity.y);
+    }
+
+    void Think()
+    {
+        nextMove = Random.Range(-1, 2);
+
+        Invoke("Think", 1);
+    }
+}
---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public float spawnTime = 3f;
+    public float curTime;
+    public Transform[] spawnPoints;
+    public GameObject enemy;
+
+    private void Update()
+    {
+        if (curTime >= spawnTime)
+        {
+            int x = Random.Range(0, spawnPoints.Length);
+            SpawnEnemy(x);
+        }
+        curTime += Time.deltaTime;
+    }
+
+    public void SpawnEnemy(int ranNum)
+    {
+        curTime = 0;
+        Instantiate(enemy, spawnPoints[ranNum]);
+    }
+}
