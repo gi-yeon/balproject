ab73444063d1c3f81091bfbda67e394e98f4e983
gunwbro <gunwbro@gmail.com>
2020-07-24 14:16:46 +0900

    add background Music

---
+++ Assets/건우/AudioScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    GameObject BackgroundMusic;
+    AudioSource backmusic;
+
+    void Awake()
+    {
+        BackgroundMusic = GameObject.Find("BackgroundMusic");
+        backmusic = BackgroundMusic.GetComponent<AudioSource>();
+
+        DontDestroyOnLoad(BackgroundMusic);
+
+    }
+}
