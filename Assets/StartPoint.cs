2f0d9400e2112a0b1343727409f15075b698f08a
jeongmo <jeongmo7264@naver.com>
2020-07-25 00:31:56 +0900

    jeongmo

---
+++ Assets/StartPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPoint : MonoBehaviour
+{
+    // ###
+    public string startPoint; //  맵이동후 케릭터 위치
+    private Moving Player; //캐릭터 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = FindObjectOfType<Moving>();
+      
+
+        if(startPoint == Player.currentMapName)
+        {
+     
+            Player.transform.position = this.transform.position;
+        }
+
+    }
+    // ###
+
+}
---
+++ Assets/TransferMap_J.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; //Scene 매니저 라이브러리 추가
+
+
+public class TransferMap_J : MonoBehaviour
+{
+    public string TransferMap; // 이동할 맵  
+
+    private Moving Player; // 케릭터
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = FindObjectOfType<Moving>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "정지")
+        {
+            Player.currentMapName = TransferMap;
+            SceneManager.LoadScene(TransferMap);
+        }
+    }
+}
